61171bcfbdcdc245e5a3b1a6d8b24cff2a4a5eb3
Ted Alff <twa022@gmail.com>
2020-03-08 21:20:04 -0400

    Initial page structure.

---
+++ script.js
@@ -0,0 +1,14 @@
+"using strict";
+
+function hamburgerMenu() {
+	$('.menu').click( function( event ) { 
+		event.preventDefault();
+		// slide out menu
+	});
+}
+
+function main() {
+	$(hamburgerMneu);
+}
+
+main();
