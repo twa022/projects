7fe784ccf2bd2707215a0732e03443ae2b25a3c0
Ted Alff <twa022@gmail.com>
2020-03-08 21:40:40 -0400

    Javascript function stubs.

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
 "using strict";
 
-function hamburgerMenu() {
+/********************************
+ *           GLOBALS            *
+ ********************************/
+
+
+/********************************
+ *          FUNCTIONS           *
+ ********************************/
+
+ function validateProjects( p ) {
+	 return true;
+ }
+
+ function generateStaticBanner() {
+	return true;
+ }
+
+ async function generateBanner() {
+	try {
+		let response = await fetch( "projects.json ");
+		if ( !response.ok ) {
+			generateStaticBanner();
+		}
+		let p = await response.json();
+		if ( !validateProjects( p ) ) {
+			generateStaticBanner();
+		}
+		// Generate the projects cards to fill the banner
+	} catch( e ) {
+		generateStaticBanner();
+	}
+
+ }
+
+
+/********************************
+ *        EVENT HANDLERS        *
+ ********************************/
+
+function hamburgerMenuHandler() {
 	$('.menu').click( function( event ) { 
 		event.preventDefault();
 		// slide out menu
 	});
 }
 
+function bannerNextHandler() {
+	$('#banner-next').click( function( event ) {
+		// Advance the banner to the next card
+	});
+}
+
+function bannerPrevHandler() {
+	$('#banner-next').click( function( event ) {
+		// Scroll the banner to the previous card
+	});
+}
+
 function main() {
-	$(hamburgerMneu);
+	// Activate all the event handlers
+	$(hamburgerMenuHandler);
+	$(bannerPrevHandler);
+	$(bannerNextHandler);
 }
 
 main();
