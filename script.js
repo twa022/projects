0c036a9dc7fb456aa64eb0a08d20e3bad8cdda0f
Ted Alff <twa022@gmail.com>
2020-03-11 11:10:44 -0400

    Import information for the projects card from the external json store. Move the logic for how many cards to display to the CSS file in media queries. Move styling for the cards to the CSS file.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
  ********************************/
 let TIMER;
 
+let STORE;
+
 /********************************
  *          FUNCTIONS           *
  ********************************/
@@ -17,43 +19,28 @@ let TIMER;
 	return true;
  }
 
- async function generateBanner() {
-	let p;
-	 /*
-	try {
-		let response = await fetch( "projects.json ");
-		if ( !response.ok ) {
-			generateStaticBanner();
-		}
-		p = await response.json();
-		if ( !validateProjects( p ) ) {
-			generateStaticBanner();
-		}
-		// Generate the projects cards to fill the banner
-	} catch( e ) {
-		generateStaticBanner();
-	}
-	*/
+async function generateBanner() {
 	let html = "";
-	let per = 1;
-	let width = $('body').width();
-	if ( width >= 900 )
-		per = 3;
-	else if ( width >= 640 )
-		per = 2;
-	for ( let i = 0 ; i < 3 ; i++ ) {
+	for ( let i = 0 ; i < STORE.projects.length ; i++ ) {
 		// Placeholder HTML to test the banner.
-		html += `<a href="test.html" _idx=${i} _per=${per} class="banner-item width-${per} banner-item-${(i%2)? "even" : "odd"}"><p style="text-align:center;font-size: 2.0rem;color: white">${i}</p></a>`;
+		html += `<a href="${STORE.projects[i].link}" _idx=${i}
+					style="color: ${STORE.projects[i].fontColor}" 
+					class="banner-item" _class="banner-item-${(i%2)? "even" : "odd"}">
+					<div class="banner-item-bgnd" style="background-image: linear-gradient( to bottom, rgba(50, 50, 50, 0.8), rgba( 100, 100, 100, 0.4) ), url('${STORE.projects[i].image}');"></div>
+					<div class="banner-item-text"><h2>${STORE.projects[i].title}</h2>${STORE.projects[i].text}</div></a>`;
 	}
-	html += `<a href="projects.html" _idx=3 _per=${per} class="banner-item width-${per} banner-item-${(3%2)? "even" : "odd"}" style="text-align:center"><p style="text-align:center;font-size: 2.0rem;color: white">All Projects <i style="text-align:center;font-size: 2.0rem;color: white" class="fas fa-list"></i></p></a>`;
-	$('.banner-slider').prepend(html);
+	html += `<a href="projects.html" _idx=${STORE.projects.length} 
+				class="banner-item" _class="banner-item-${(3%2)? "even" : "odd"}">
+				<div class="banner-item-bgnd" style="background-image: linear-gradient( to bottom, rgba(50, 50, 50, 0.8), rgba( 100, 100, 100, 0.4) ), url('images/all_projects.png');"></div>
+				<div class="banner-item-text"><h2>All Projects</h2> </div></a>`;
+	$('.banner-slider').html(html);
  }
 
  function slideNext() {
 	console.log('sliding to next');
 	stopSlideshow();
 	$('.banner-slider').animate(
-		{ left: `-=${100/Number($('.banner-item:first-child').attr('_per'))}%` },
+		{ left: `-=${Number($('.banner-item:first-child').width())}px` },
 		1200, 
 		function() {
 			$('.banner-item:last-child').after($('.banner-item:first-child'));
@@ -66,10 +53,11 @@ let TIMER;
 function slidePrev() {
 	console.log('sliding to previous');
 	stopSlideshow();
+	let width = Number($('.banner-item:first-child').width());
 	$('.banner-item:first-child').before($('.banner-item:last-child'));
-	$('.banner-slider').css({'left': `-${100/Number($('.banner-item:first-child').attr('_per'))}%`});
+	$('.banner-slider').css({'left': `${ -1 * width }px`} );
 	$('.banner-slider').animate(
-		{ left: `+=${100/Number($('.banner-item:first-child').attr('_per'))}%` },
+		{ left: `+=${width}` },
 		1200, 
 		function() { $('.banner-slider').css('left', '0%'); }
 	);
@@ -82,6 +70,21 @@ function startSlideshow() {
 
 function stopSlideshow() {
 	clearInterval( TIMER );
+}
+
+async function loadStore( file ) {
+	try {
+		let response = await fetch( file );
+		if ( !response.ok ) { 
+			console.log( `response.ok is not okay!`);
+			return;
+		}
+		STORE = await response.json();
+	} catch ( e ) {
+		console.log( 'Caught an error ' + e.message );
+		STORE = null;
+		return;
+	}
 }
 
 /********************************
@@ -109,28 +112,14 @@ function bannerPrevHandler() {
 	});
 }
 
-function resizeHandler() {
-	$(window).resize( function( event ) {
-		let per = 1;
-		let width = $('body').width();
-		if ( width >= 900 )
-			per = 3;
-		else if ( width >= 640 )
-			per = 2;
-		for ( let i = 1 ; i < 4 ; i++ ) {
-			$('.banner-item').removeClass(`width-${i}`);
-		}
-		$('.banner-item').addClass(`width-${per}`);
-		$('.banner-item').attr('_per', `${per}`);
-	});
-}
-
-function main() {
+async function main() {
 	// Activate all the event handlers
 	$(hamburgerMenuHandler);
 	$(bannerPrevHandler);
 	$(bannerNextHandler);
-	$(resizeHandler);
+
+	await loadStore( 'store.json' );
+	console.log( STORE.projects[0].title );
 
 	generateBanner();
 
