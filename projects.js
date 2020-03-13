7092c11e9f27ab58401fe94125b995de34e6bc54
Ted Alff <twa022@gmail.com>
2020-03-13 13:41:54 -0400

    Initial projects javascript file to fill content.

---
+++ projects.js
@@ -0,0 +1,22 @@
+"using strict";
+
+function displayProjects() {
+	let html = '';
+	STORE.projects.forEach( function( project ) {
+		html += `
+		<div class="project">
+			<h3><a href="${project.link}>${project.title}</a></h3>
+			<img src="${project.image}" alt="${project.title} picture">
+			${project.text}
+		</div>`
+	});
+	$('.projects').append( html );
+}
+
+async function main() {
+	await commonMain();
+
+	displayProjects();
+}
+
+main();
